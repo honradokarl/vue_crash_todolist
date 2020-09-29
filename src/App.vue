1bf01686515a68ccc9ade2cbfcef62b43fd14bb0
Karl John Honrado <honradokarljohn@gmail.com>
2020-09-29 18:12:05 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <div id="app">
+    <Header />
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Header from './components/layout/Header';
+
+  export default {
+    name: "app",
+    components: {
+      Header
+    }
   }
-}
+
 </script>
 
 <style>
@@ -21,6 +29,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList app v1.0.0. It is part of the Vue crash to do list.
+    </p>
+  </div>
+</template>
